df20b0b146835a5c396ee6a839e24caf98601932
Ubuntu <ubuntu@epdam5mq4rqki4418n0n.auto.internal>
2026-06-07 11:53:13 +0000

    feat: change install sh to build mmcv

--- install.sh
+++ install.sh
@@ -4,21 +4,25 @@ set -e
 ENV_TYPE=${1:-cpu}
 
 echo "=== 1. Синхронизация базового окружения ($ENV_TYPE) ==="
-uv sync --extra "$ENV_TYPE" --system-certs
+uv sync --extra "$ENV_TYPE"
 
 if [ "$ENV_TYPE" = "gpu" ]; then
-  echo "=== 2. Установка MMCV для GPU (CUDA 12.1) ==="
-  uv pip install mmcv==2.1.0 \
-    -f https://download.openmmlab.com/mmcv/dist/cu121/torch2.1/index.html \
-    --system-certs --no-build-isolation
+  echo "=== 2. Установка MMCV для GPU ==="
+  wget https://github.com/open-mmlab/mmcv/archive/refs/tags/v2.1.0.tar.gz
+  tar -xvf v2.1.0.tar.gz
+  cd mmcv-2.1.0
+  uv pip install -r requirements/optional.txt
+  CUDA_HOME=/usr/local/cuda-12.1 FORCE_CUDA=1 uv pip install -e . --no-build-isolation -v
+  uv run python .dev_scripts/check_installation.py
+  cd ..
 else
   echo "=== 2. Установка MMCV для CPU ==="
   uv pip install mmcv==2.1.0 \
     -f https://download.openmmlab.com/mmcv/dist/cpu/torch2.1/index.html \
-    --system-certs --no-build-isolation
+    --no-build-isolation
 fi
 
 echo "=== 3. Установка локального mmdetection ==="
-uv --system-certs pip install ./mmdetection --no-build-isolation
+uv pip install ./mmdetection --no-build-isolation
 
 echo "🎉 Окружение успешно настроено!"
